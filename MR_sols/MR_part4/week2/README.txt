8d55ff6c8467aadc933ae77fa13ab8375929fdea
Arpit <123756415+ar-py-grim@users.noreply.github.com>
2025-07-31 20:30:28 +0530

    Delete MR_sols directory

--- MR_sols/MR_part4/week2/README.txt
+++
@@ -1,3 +0,0 @@
-Sampling-Based Planning
-
-A Rapidly exploring Random Trees (RRT) is used to create a path
--- MR_sols/graded_assignment_week3.py
+++
@@ -1,8 +0,0 @@
-import modern_robotics as mr
-import numpy as np
-
-print(mr.QuinticTimeScaling(Tf=5,t=3))
-X_sart = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-X_end = np.array([[0,0,1,1],[1,0,0,2],[0,1,0,3],[0,0,0,1]])
-print(f"for method=3: {mr.ScrewTrajectory(Xstart=X_sart,Xend=X_end,Tf=10,N=10,method=3)}")
-print(f"for method=5: {mr.CartesianTrajectory(Xstart=X_sart,Xend=X_end,Tf=10,N=10,method=5)}")
